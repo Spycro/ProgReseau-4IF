15bfcf115503628fcee14f7bd6d30dd9c357e739
Spycro <lucaszim16@gmail.com>
2020-11-16 09:36:15 +0100

    moved main

--- src/main/java/fr/insalyon/stream/ChatServer.java
+++ src/main/java/fr/insalyon/stream/ChatServer.java
@@ -53,10 +53,14 @@ public class ChatServer  {
 		}
 	}
 
-	public static void main(String args[]){ 
-		ServerSocket listenSocket;
-
-
+	public static void main(String args[]) {
+		if (args.length != 1) {
+			System.out.println("Usage: java EchoServer <EchoServer port>");
+			System.exit(1);
+		}
+		int port = Integer.parseInt(args[0]);
+		ChatServer chatServer = new ChatServer(port);
+		chatServer.LaunchServer();
 	}
 }
 
